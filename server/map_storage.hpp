300c722200bb05c8a7cee80b777f42cc347a2d1d
liny77 <lin.y77@qq.com>
2018-09-23 15:49:01 +0800

    completed milestone2

--- server/map_storage.hpp
+++ server/map_storage.hpp
@@ -19,11 +19,15 @@ class MapStorage : public AbstractStorage {
     auto typed_vals = third_party::SArray<Val>(vals);
     CHECK_EQ(typed_keys.size(), typed_vals.size());
     // TODO
+    for (int i = 0; i < typed_keys.size(); ++i) {
+      storage_[typed_keys[i]] = typed_vals[i];
+    }
   }
 
   virtual third_party::SArray<char> SubGet(const third_party::SArray<Key>& typed_keys) override {
     third_party::SArray<Val> reply_vals(typed_keys.size());
     // TODO
+    for (int i = 0; i < typed_keys.size(); ++i) reply_vals[i] = storage_.find(typed_keys[i])->second;
     return third_party::SArray<char>(reply_vals);
   }
 
--- server/server_thread.cpp
+++ server/server_thread.cpp
@@ -5,12 +5,34 @@
 namespace csci5570 {
 
 void ServerThread::RegisterModel(uint32_t model_id, std::unique_ptr<AbstractModel>&& model) {
+    // insert if key is not existed, otherwise do nothing
+    models_.insert(std::make_pair(model_id, std::move(model)));
 }
 
 AbstractModel* ServerThread::GetModel(uint32_t model_id) {
+    // return model if existed, otherwise return nullptr
+    auto it = models_.find(model_id);
+    return (it != models_.end()) ? it->second.get() : nullptr;
 }
 
 void ServerThread::Main() {
+    Message msg;
+    while (true) {
+        work_queue_.WaitAndPop(&msg);
+        if (msg.meta.flag == Flag::kExit) break;
+        auto *model = GetModel(msg.meta.model_id);
+        switch (msg.meta.flag) {
+            case Flag::kClock:
+                model->Clock(msg);
+                break;
+            case Flag::kAdd:
+                model->Add(msg);
+                break;
+            case Flag::kGet:
+                model->Get(msg);
+                break;
+        }
+    }
 }
 
 }  // namespace csci5570
