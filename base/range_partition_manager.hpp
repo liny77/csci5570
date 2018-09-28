3193a3683b6779239e7f456774589d46b39cb6a1
liny77 <lin.y77@qq.com>
2018-09-28 16:27:28 +0800

    implement partition manager

--- base/range_partition_manager.hpp
+++ base/range_partition_manager.hpp
@@ -14,11 +14,49 @@ namespace csci5570 {
 class RangePartitionManager : public AbstractPartitionManager {
  public:
   RangePartitionManager(const std::vector<uint32_t>& server_thread_ids, const std::vector<third_party::Range>& ranges)
-      : AbstractPartitionManager(server_thread_ids) {}
+      : AbstractPartitionManager(server_thread_ids), ranges_(ranges.begin(), ranges.end()) {}
 
-  void Slice(const Keys& keys, std::vector<std::pair<int, Keys>>* sliced) const override {}
+  void Slice(const Keys& keys, std::vector<std::pair<int, Keys>>* sliced) const override {
+      std::map<int, Keys> resultMap;// server id, keys
+      for (auto key : keys) {
+          for (int i = 0; i < ranges_.size(); ++i) {
+              if (ranges_[i].begin() <= key && key < ranges_[i].end()) {//  begin <= key < end
+                  if (resultMap.find(server_thread_ids_[i]) == resultMap.end()) {// server id does not exist
+                      resultMap[server_thread_ids_[i]] = Keys();
+                  }
+                  resultMap.find(server_thread_ids_[i])->second.push_back(key);
+                  break;
+              }
+          }
+      }
+      for (auto it = resultMap.begin(); it != resultMap.end(); it++) {// collect slices
+          sliced->push_back(std::make_pair(it->first, it->second));
+      }
+  }
 
-  void Slice(const KVPairs& kvs, std::vector<std::pair<int, KVPairs>>* sliced) const override {}
+  void Slice(const KVPairs& kvs, std::vector<std::pair<int, KVPairs>>* sliced) const override {
+      std::map<int, KVPairs> resultMap;// server id, keys
+      for (int k = 0; k < kvs.first.size(); ++k) {
+          Key key = kvs.first[k];// get current key
+          double value = kvs.second[k];// get current value
+          for (int i = 0; i < ranges_.size(); ++i) {
+              if (ranges_[i].begin() <= key && key < ranges_[i].end()) {//  begin <= key < end
+                  if (resultMap.find(server_thread_ids_[i]) == resultMap.end()) {// server id does not exist
+                      KVPairs toAdd;
+                      toAdd.first = Keys();
+                      toAdd.second = third_party::SArray<double>();
+                      resultMap[server_thread_ids_[i]] = toAdd;
+                  }
+                  resultMap.find(server_thread_ids_[i])->second.first.push_back(key);
+                  resultMap.find(server_thread_ids_[i])->second.second.push_back(value);
+                  break;
+              }
+          }
+      }
+      for (auto it = resultMap.begin(); it != resultMap.end(); it++) {// collect slices
+          sliced->push_back(std::make_pair(it->first, it->second));
+      }
+  }
 
  private:
   std::vector<third_party::Range> ranges_;
